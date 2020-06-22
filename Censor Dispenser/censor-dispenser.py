8cbf7324600ae264f1199e368539b2ca18a2626e
potatoscones <65110395+potatoscones@users.noreply.github.com>
2020-06-22 23:03:09 +0100

    Added control flow, comments, whole word options

--- Censor Dispenser/censor-dispenser.py
+++ Censor Dispenser/censor-dispenser.py
@@ -12,11 +12,22 @@ negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "ala
 
 test_list = ['blorb', 'flork', 'chees']
 
-def findall(text, phrase):
-    i = text.find(phrase)
-    while i != -1:
-        yield i
-        i = text.find(phrase, i+1)
+
+# Generator function to find every occurrence of a given phrase:
+
+def findall(text, phrase, check_end = False):                                                       # Last flag optional toggle to disregard whether the term makes a whole word
+    phrase_length = len(phrase)
+    i = text.find(phrase)                                                                             
+    while i != -1:                                                                                  # If phrase found:
+        if check_end == True:
+            phrase_end = text[i+phrase_length] 
+            if phrase_end in string.punctuation or phrase_end == ' ' or phrase_end == '\n':         # Check to make sure it's a whole word (i.e. with punctuation or whitespace after)
+                yield i                                                                             # Add to the iterator
+        else:
+            yield i
+        i = text.find(phrase, i+1)                                                                  # Next time, search after the found phrase
+
+# Base censor function to replace all non-punctutation/whitespace characters with '#'
 
 def censor(text):
     new_text = ''
@@ -31,15 +42,19 @@ def censor(text):
             new_text += '#'
     return new_text
 
+# Function 1 - Censor a given phrase from text
+
 def censor_phrase(text, phrase):
-    text_casefold = text.casefold()
+    text_casefold = text.casefold()                                                                 # Ignore case of text and phrase
     phrase_casefold = phrase.casefold()
     phrase_length = len(phrase)
-    start_index = text_casefold.find(phrase_casefold)        
-    end_index = start_index + phrase_length
-    if start_index >=0:
-        text = text[:start_index] + censor(text[start_index:end_index]) + text[end_index:]
+    for start_index in findall(text_casefold, phrase_casefold):                                     # Locate each instance of phrase
+        end_index = start_index + phrase_length                                                   
+        if start_index >=0:
+            text = text[:start_index] + censor(text[start_index:end_index]) + text[end_index:]      # Replace phrase using censor function
     return text
+
+# Function 2 - Censor all phrases in given list from text
 
 def censor_from_list(text, phrase_list):
     text_casefold = text.casefold()
@@ -47,36 +62,38 @@ def censor_from_list(text, phrase_list):
         phrase_casefold = phrase.casefold()
         phrase_length = len(phrase)
         for start_index in findall(text_casefold, phrase_casefold):
-            end_index = start_index + phrase_length
+            end_index = start_index + phrase_length                                          
             if start_index >=0:
                 text = text[:start_index] + censor(text[start_index:end_index]) + text[end_index:]
     return text
 
+# Function 3 - Censor all phrases in given list from text, AFTER the first two occurrences of any phrase
+
 def censor_after_two(text, phrase_list):
     text_casefold = text.casefold()
     index_list = []
-    for phrase in phrase_list:
+    for phrase in phrase_list:                                                                      # First pass to find each phrase in the text
         phrase_casefold = phrase.casefold()
         phrase_length = len(phrase)
         for start_index in findall(text_casefold, phrase_casefold):
             if start_index >= 0:
-                index_list.append(start_index+phrase_length)                                    #append the end index of word if found
-    index_list.sort()                                                                       #sort all of the end indices
-    if index_list:
-        untouched_text = text[:index_list[1]]
-        censored_text = text[index_list[1]:]                                                #if elements in list, choose text to censor and leave
+                index_list.append(start_index+phrase_length)                                        # Append the end index of phrase if found
+    index_list.sort()                                                                               # Sort all of the end indices
+    if index_list:                                                                                  # If there are any phrases in the text:
+        untouched_text = text[:index_list[1]]                                                       # Decide which text to leave behind
+        censored_text = text[index_list[1]:]                                                        # And which to censor
         censored_text_casefold = censored_text.casefold()
         for phrase in phrase_list:
             phrase_casefold = phrase.casefold()
             phrase_length = len(phrase)
-            for start_index in findall(text_casefold, phrase_casefold):
-                end_index = start_index + phrase_length
+            for start_index in findall(censored_text_casefold, phrase_casefold):                    
+                end_index = start_index + phrase_length                                    
                 if start_index > 0:
                     censored_text = censored_text[:start_index] + censor(censored_text[start_index:end_index]) + censored_text[end_index:]
         text = untouched_text + censored_text
     return text
 
-
+# Function 4 - Censor all phrases in fiven list from text, AND each word either side of the phrase
 
 def censor_either_side(text, phrase_list):
     text_casefold = text.casefold()
@@ -84,23 +101,60 @@ def censor_either_side(text, phrase_list):
         phrase_casefold = phrase.casefold()
         phrase_length = len(phrase)
         if phrase_casefold in text_casefold:
-            for start_index in findall(text_casefold, phrase_casefold):
-                end_index = start_index + phrase_length
-                start_space_count = 0
+            for start_index in findall(text_casefold, phrase_casefold, True):                       # Don't catch words within words
+                end_index = start_index + phrase_length                                           
+                start_space_count = 0                                                               # Initialise start and end space count
                 end_space_count = 0
-                while start_index > 0 and start_space_count < 2:             # from start_index go backwards until previous space
+                while start_index > 0 and start_space_count < 2:                                    # From start_index, go backwards until two spaces are found (the space before our target word, and the space before the word before that...)
                     start_index -= 1
                     if text[start_index] == ' ':
                         start_space_count += 1
-                while end_index < len(text) - 1 and end_space_count < 1:     # from end_index go forwards until next space
+                while end_index < len(text) - 1 and end_space_count < 1:                            # From end_index, go forwards until two spaces are found (the space after our target word, and the space after the word after that...)
                     end_index += 1
                     if text[end_index] == ' ':
                         end_space_count += 1
                 text = text[:start_index] + censor(text[start_index:end_index]) + text[end_index:]
     return text
 
-print(censor_phrase(email_one, 'learning algorithms'))
-print(censor_from_list(email_two, proprietary_terms))
-print(censor_after_two(email_three, negative_words))
-print(censor_either_side(email_four, proprietary_terms + negative_words))
+# print(censor_phrase(email_one, 'learning algorithms'))
+# print(censor_from_list(email_two, proprietary_terms))
+# print(censor_after_two(email_three, negative_words))
+# print(censor_either_side(email_four, proprietary_terms + negative_words))
+def function_choice():
+    user_choice = input('''
+    Choose from the four test functions:
+    1 Censor a given phrase from text
+    2 Censor all phrases in given list from text
+    3 Censor all phrases in given list from text, AFTER the first two occurrences of any phrase
+    4 Censor all phrases in given list from text, AND each word either side of the phrase
+    \n''')
+    if user_choice == '1':
+        print('\n', censor_phrase(choose_email(), 'learning algorithm'))
+    elif user_choice == '2':
+        print('\n', censor_from_list(choose_email(), proprietary_terms))
+    elif user_choice == '3':
+        print('\n', censor_after_two(choose_email(), negative_words))
+    elif user_choice == '4':
+        print('\n', censor_either_side(choose_email(), proprietary_terms + negative_words))
+
+def choose_email():
+    user_choice = input('''
+    Select sample text to censor:
+    1 Email one
+    2 Email two
+    3 Email three
+    4 Email four
+    \n''')
+    if user_choice == '1':
+        return email_one
+    elif user_choice == '2':
+        return email_two
+    elif user_choice == '3':
+        return email_three
+    elif user_choice == '4':
+        return email_four
+    else:
+        choose_email()
+
+function_choice()
 
